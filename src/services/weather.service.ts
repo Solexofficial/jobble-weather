963083674972bbc3f8787b3a5c537a53695812d4
Mark Pokrovsky <solexofficial@yandex.ru>
2022-04-01 20:25:37 +0300

    refactor: js to ts

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -5,7 +5,7 @@ const weatherApiEndPoint = '/weather';
 const apiKey = '92c5e50e06d29a7db7f3925cfb71bd7a';
 
 const weatherService = {
-  get: async query => {
+  get: async (query: string) => {
     const { data } = await httpService.get(weatherApiEndPoint, {
       params: {
         q: query,
