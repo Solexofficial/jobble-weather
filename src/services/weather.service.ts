fa01656e533820de78f0fe16eb3a0a58b98f55fb
Mark Pokrovsky <solexofficial@yandex.ru>
2022-04-01 20:37:19 +0300

    refactor: add enviroment variables

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -2,15 +2,13 @@ import httpService from './http.service';
 
 const weatherApiEndPoint = '/weather';
 
-const apiKey = '92c5e50e06d29a7db7f3925cfb71bd7a';
-
 const weatherService = {
   get: async (query: string) => {
     const { data } = await httpService.get(weatherApiEndPoint, {
       params: {
         q: query,
         units: 'metric',
-        APPID: apiKey,
+        APPID: process.env.REACT_APP_API_KEY,
       },
     });
 
